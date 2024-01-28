b47b2372c22a7807d8a3ff76201018e714631b13
ANEGI2811 <aashishnegi0021@gmail.com>
2024-01-28 14:28:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Banner from './Components/Banner';
+import Favourites from './Components/Favourites';
+import MovieList from './Components/MovieList';
+import NavBar from './Components/NavBar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+
+    <NavBar/>
+
+    <Routes>
+      <Route path='/' element={<> <Banner/> <MovieList/> </>} />
+      <Route path='/favourites' element={<Favourites/>} />
+    </Routes>
+    
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
